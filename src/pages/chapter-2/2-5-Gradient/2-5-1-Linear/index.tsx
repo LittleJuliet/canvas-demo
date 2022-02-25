634069b1aad4949de8d1518a766b9902fb7709e2
huainan <huainan.luo@tuya.com>
2022-02-25 17:58:52 +0800

    feat: add some section

--- src/pages/chapter-2/2-5-Gradient/2-5-1-Linear/index.tsx
+++ src/pages/chapter-2/2-5-Gradient/2-5-1-Linear/index.tsx
@@ -5,7 +5,50 @@ function Linear() {
   useEffect(init, []);
 
   function init() {
-    const canvas = document.querySelectorAll('#')
+    const canvasCollection = document.getElementsByTagName('canvas');
+    const canvasArr: HTMLCanvasElement[] = [];
+    const ctxArr: CanvasRenderingContext2D[] = [];
+    for (let i = 0; i < canvasCollection.length; i++) {
+      const tempCanvas = canvasCollection.item(i) as HTMLCanvasElement;
+      canvasArr.push(tempCanvas);
+      ctxArr.push(tempCanvas.getContext('2d') as CanvasRenderingContext2D);
+    }
+
+    let gradient = ctxArr[0].createLinearGradient(0, 0, canvasArr[0].width, 0);
+    gradient.addColorStop(0, '#0000ff');
+    gradient.addColorStop(0.25, '#fff');
+    gradient.addColorStop(0.5, 'purple');
+    gradient.addColorStop(0.75, '#ff0000');
+    gradient.addColorStop(1, '#ffff00');
+    ctxArr[0].fillStyle = gradient;
+    ctxArr[0].fillRect(0, 0, canvasArr[0].width, canvasArr[0].height);
+
+    gradient = ctxArr[1].createLinearGradient(0, 0, 0, canvasArr[1].height);
+    gradient.addColorStop(0, '#0000ff');
+    gradient.addColorStop(0.25, '#fff');
+    gradient.addColorStop(0.5, 'purple');
+    gradient.addColorStop(0.75, '#ff0000');
+    gradient.addColorStop(1, '#ffff00');
+    ctxArr[1].fillStyle = gradient;
+    ctxArr[1].fillRect(0, 0, canvasArr[1].width, canvasArr[1].height);
+
+    gradient = ctxArr[2].createLinearGradient(0, 0, 0, canvasArr[2].height / 2);
+    gradient.addColorStop(0, '#0000ff');
+    gradient.addColorStop(0.25, '#fff');
+    gradient.addColorStop(0.5, 'purple');
+    gradient.addColorStop(0.75, '#ff0000');
+    gradient.addColorStop(1, '#ffff00');
+    ctxArr[2].fillStyle = gradient;
+    ctxArr[2].fillRect(0, 0, canvasArr[2].width, canvasArr[2].height);
+
+    gradient = ctxArr[3].createLinearGradient(0, 0, canvasArr[3].width, canvasArr[3].height);
+    gradient.addColorStop(0, '#0000ff');
+    gradient.addColorStop(0.25, '#fff');
+    gradient.addColorStop(0.5, 'purple');
+    gradient.addColorStop(0.75, '#ff0000');
+    gradient.addColorStop(1, '#ffff00');
+    ctxArr[3].fillStyle = gradient;
+    ctxArr[3].fillRect(0, 0, canvasArr[3].width, canvasArr[3].height);
   }
 
   return (
